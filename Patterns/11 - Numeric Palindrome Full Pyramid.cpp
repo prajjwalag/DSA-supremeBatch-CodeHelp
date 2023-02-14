3d4d8d6989b60201ed54cb51b5fc5383cc282a6c
prajjwalag <50009834+prajjwalag@users.noreply.github.com>
2023-02-15 00:43:28 +0530

    Create 11 - Numeric Palindrome Full Pyramid.cpp

---
+++ Patterns/11 - Numeric Palindrome Full Pyramid.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int size;
+  cout<<"Enter the size: ";
+  cin>>size;
+  int element = 1;
+  for (int i = 0; i < size; i++) {
+    element=i+1;
+    for(int j = 0; j < size-i-1; j++) {
+        cout<<"  ";
+    }
+    for(int j=0; j <= i; j++, element++) {
+      cout<<element<<" ";
+    }
+    element-=2;
+    for(int j=0; j < i; j++, element--) {
+      cout<<element<<" ";
+    }
+    cout<<endl;
+  }
+}
