8494a2fde83de8e31fce327c0facf1118a745309
prajjwalag <50009834+prajjwalag@users.noreply.github.com>
2023-02-15 00:04:36 +0530

    Create 07 - Numbers Half Pyramid.cpp

---
+++ Patterns/07 - Numbers Half Pyramid.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int size;
+  cout<<"Enter the size: ";
+  cin>>size;
+  for (int i = 0; i < size; i++) {
+    for(int j = 0; j <= i; j++) {
+        cout<<j+1<<" ";
+    }
+    cout<<endl;
+  }
+}
