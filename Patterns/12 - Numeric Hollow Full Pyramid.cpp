2cf5a5de584f3fe19705fb45be4d38b7fa7c0eca
prajjwalag <50009834+prajjwalag@users.noreply.github.com>
2023-02-15 00:48:25 +0530

    Create 12 - Numeric Hollow Full Pyramid.cpp

---
+++ Patterns/12 - Numeric Hollow Full Pyramid.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int size;
+  cout<<"Enter the size: ";
+  cin>>size;
+  for (int i = 0; i < size; i++) {
+    for(int j = 0; j < size-i-1; j++) {
+        cout<<" ";
+    }
+    for(int j=0; j <= i; j++) {
+      if(j==0 || j==i || i == size-1) {
+        cout<<j+1<<" ";
+      } else {
+        cout<<"  ";
+      } 
+    }
+    cout<<endl;
+  }
+}
