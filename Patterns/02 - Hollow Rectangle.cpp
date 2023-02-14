a4312a8cdbbaffde6a32e3a5f378cccfa27db352
prajjwalag <50009834+prajjwalag@users.noreply.github.com>
2023-02-14 23:54:17 +0530

    Create 02 - Hollow Rectangle.cpp

---
+++ Patterns/02 - Hollow Rectangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int row, col;
+  cout<<"Enter the number of columns: ";
+  cin>>col;
+  cout<<"Enter the numer of rows: ";
+  cin>>row;
+  for (int i = 0; i < row; i++) {
+    for(int j = 0; j < col; j++) {
+      if(i == 0 || j == 0 || i == row-1 || j == col-1 ) {
+        cout<<"* ";
+      } else {
+        cout<<"  ";
+      }
+    }
+    cout<<endl;
+  }
+}
