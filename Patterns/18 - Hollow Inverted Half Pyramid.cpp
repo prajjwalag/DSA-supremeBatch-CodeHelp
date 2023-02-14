e475e987196243994d5c42da0322f83bb7748052
prajjwalag <50009834+prajjwalag@users.noreply.github.com>
2023-02-15 01:14:45 +0530

    Create 18 - Hollow Inverted Half Pyramid.cpp

---
+++ Patterns/18 - Hollow Inverted Half Pyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int size;
+  cout<<"Enter the size: ";
+  cin>>size;
+  for (int i = 0; i < size; i++) {
+    for(int j = 0; j <= size-i-1; j++) {
+      if(j==0 || i==0 || j==size-i-1){
+        cout<<"* ";
+      }
+      else {
+        cout<<"  ";
+      }
+    }
+    cout<<endl;
+  }
+}
